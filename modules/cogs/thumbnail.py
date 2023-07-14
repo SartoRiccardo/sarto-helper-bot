d6205f3bd6363bceab93b407c0874e4067f038cc
SartoRiccardo <rikki.sarto@gmail.com>
2023-07-14 12:51:02 +0200

    Slash commands - /thumbnail

--- modules/cogs/thumbnail.py
+++ modules/cogs/thumbnail.py
@@ -1,5 +1,4 @@
 import os
-import re
 import discord
 import importlib
 from random import randint
@@ -8,6 +7,7 @@ import modules.data
 import modules.data.owner
 import modules.data.reditor
 import modules.util
+from typing import Optional, Literal
 pgsql = modules.data
 util = modules.util
 
@@ -16,6 +16,8 @@ SUCCESS_REACTION = '\N{THUMBS UP SIGN}'
 
 
 class ThumbnailCog(commands.Cog):
+    thumbnail = discord.app_commands.Group(name="thumbnail", description="Various commands to create thumbnails.")
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -28,55 +30,31 @@ class ThumbnailCog(commands.Cog):
         importlib.reload(pgsql)
 
     @staticmethod
-    async def fetch_thumb_image_url(ctx):
-        if len(ctx.message.attachments) == 0:
-            # Fallback if no image attached
-            if not ctx.message.reference:
-                # Use last image sent in the channel by a non-bot user
-                thumb_img_url = None
-                history = [msg async for msg in ctx.channel.history(limit=100)]
-                for message in history:
-                    if not message.author.bot and len(message.attachments) > 0:
-                        return message.attachments[0].url
+    async def fetch_thumb_image_url(interaction: discord.Interaction) -> str or None:
+        # Use last image sent in the channel by a non-bot user
+        history = [msg async for msg in interaction.channel.history(limit=100)]
+        for message in history:
+            if not message.author.bot and len(message.attachments) > 0:
+                return message.attachments[0].url
+        return None
 
-                if not thumb_img_url:
-                    await ctx.send("You must attach an image!")
-                    return
-
-            # If replying to a message, use that message's image, if any.
-            reference = ctx.message.reference
-            if reference.cached_message:
-                message = reference.cached_message
-            else:
-                message = await ctx.channel.fetch_message(reference.message_id)
-                if not message:
-                    await ctx.send("Are you replying to a message that's too old? Please send the image again!")
-                    return
-            return message.attachments[0].url
-        return ctx.message.attachments[0].url
-
-    @commands.group(aliases=["thmb"], invoke_without_command=True)
-    async def thumbnail(self, ctx, *, thumb_text):
-        if ctx.invoked_subcommand is not None:
-            return
-
-        thumb_img_url = await self.fetch_thumb_image_url(ctx)
+    @thumbnail.command(name="create", description="Turn the latest image sent into a thumbnail")
+    @discord.app_commands.rename(thumb_text="text")
+    @discord.app_commands.describe(thumb_text="The thumbnail text.")
+    async def create(self,
+                     interaction: discord.Interaction,
+                     thumb_text: str,
+                     with_backdrop: Optional[bool] = True,
+                     watermark: Optional[bool] = False):
+        thumb_img_url = await self.fetch_thumb_image_url(interaction)
         if thumb_img_url is None:
             return
 
-        if not (thumb_img_url.endswith(".png") or thumb_img_url.endswith(".jpg")):
-            await ctx.send("You must attach an image!")
-            return
-
-        backdrop = util.image.BLACK
-        if "-b" in thumb_text:
-            thumb_text = thumb_text.replace("-b", "")
-            backdrop = None
-
-        watermark = False
-        if "-w" in thumb_text:
-            thumb_text = thumb_text.replace("-w", "")
-            watermark = True
+        await interaction.response.send_message(
+            content="Working on it...",
+            ephemeral=True
+        )
+        backdrop = util.image.BLACK if with_backdrop else None
 
         tmp_path = os.path.abspath(os.path.dirname(__file__)) + "/../../tmp"
         if not os.path.exists(tmp_path):
@@ -89,44 +67,25 @@ class ThumbnailCog(commands.Cog):
         await util.requests.download_file(thumb_img_url, source_path)
         util.image.make_thumbnail(thumb_text, source_path, dest_path, backdrop=backdrop,
                                   put_watermark=watermark)
-        await self.send_and_rm(source_path, dest_path, ctx)
+        await self.send_and_rm(source_path, dest_path, interaction)
 
-    @thumbnail.command()
-    async def circle(self, ctx, *args):
-        thumb_img_url = await self.fetch_thumb_image_url(ctx)
-        if thumb_img_url is None:
-            return
-
-        if not (thumb_img_url.endswith(".png") or thumb_img_url.endswith(".jpg")):
-            await ctx.send("You must attach an image!")
-            return
+    @thumbnail.command(name="circle", description="Add a circle in a thumbnail.")
+    @discord.app_commands.describe(position="The circle's position",
+                                   size="The circle's radius")
+    async def circle(self,
+                     interaction: discord.Interaction,
+                     position: Optional[Literal["left", "center", "right"]] = "center",
+                     size: Optional[Literal["1", "2", "3", "4"]] = "4"):
+        thumb_img_url = await self.fetch_thumb_image_url(interaction)
+        await interaction.response.send_message(
+            content="Working on it...",
+            ephemeral=True
+        )
 
         circle_config = {
-            "r": "4",
-            "x": "c",
+            "r": size,
+            "x": position[0],
         }
-        allowed_values = {
-            "r": [str(n+1) for n in range(4)] + ["sm", "md", "lg", "xl"],
-            "x": ["l", "c", "r"],
-        }
-
-        regex = r"(.+)(?:=|:|-)(.+)"
-        for arg in args:
-            arg = arg.lower()
-
-            match = re.search(regex, arg)
-            if match is None:
-                for key in allowed_values:
-                    if arg in allowed_values[key]:
-                        circle_config[key] = arg
-                        break
-                continue
-
-            key = match.group(1)
-            val = match.group(2)
-            if key in circle_config and val in allowed_values[key]:
-                circle_config[key] = val
-
         tmp_path = os.path.abspath(os.path.dirname(__file__)) + "/../../tmp"
         if not os.path.exists(tmp_path):
             os.mkdir(tmp_path)
@@ -137,40 +96,31 @@ class ThumbnailCog(commands.Cog):
 
         await util.requests.download_file(thumb_img_url, source_path)
         util.image.add_circle(source_path, dest_path, **circle_config)
-        await self.send_and_rm(source_path, dest_path, ctx)
+        await self.send_and_rm(source_path, dest_path, interaction)
 
-    @thumbnail.command()
-    async def arrow(self, ctx, arg0="cc", arg1="md"):
-        thumb_img_url = await self.fetch_thumb_image_url(ctx)
-        if thumb_img_url is None:
-            return
+    @thumbnail.command(name="arrow", description="Add an arrow to a thumbnail.")
+    @discord.app_commands.describe(position_x="The arrow's horizontal pointing position",
+                                   position_y="The arrow's vertical pointing position",
+                                   size="The arrow's size")
+    async def arrow(self,
+                    interaction: discord.Interaction,
+                    position_x: Optional[Literal["left", "center", "right"]] = "center",
+                    position_y: Optional[Literal["top", "center", "bottom"]] = "center",
+                    size: Optional[Literal["small", "medium", "large"]] = "medium"):
+        thumb_img_url = await self.fetch_thumb_image_url(interaction)
+        await interaction.response.send_message(
+            content="Working on it...",
+            ephemeral=True
+        )
 
-        if not (thumb_img_url.endswith(".png") or thumb_img_url.endswith(".jpg")):
-            await ctx.send("You must attach an image!")
-            return
-
+        allowed_values_transform = {
+            "small": "sm", "medium": "md", "large": "lg",
+        }
         arrow_config = {
-            "x": "c",
-            "y": "c",
-            "size": "md"
+            "x": position_x[0],
+            "y": position_y[0],
+            "size": allowed_values_transform[size],
         }
-        allowed_values = {
-            "x": ["l", "c", "r"],
-            "y": ["t", "c", "b"],
-            "size": ["sm", "md", "lg"],
-        }
-
-        if arg1 in allowed_values["size"]:
-            arrow_config["size"] = arg1
-
-        if arg0 in allowed_values["size"]:
-            arrow_config["size"] = arg0
-        else:
-            for char in arg0:
-                if char in allowed_values["x"] and char != "c":
-                    arrow_config["x"] = char
-                elif char in allowed_values["y"] and char != "c":
-                    arrow_config["y"] = char
 
         tmp_path = os.path.abspath(os.path.dirname(__file__)) + "/../../tmp"
         if not os.path.exists(tmp_path):
@@ -182,19 +132,19 @@ class ThumbnailCog(commands.Cog):
 
         await util.requests.download_file(thumb_img_url, source_path)
         util.image.add_arrow(source_path, dest_path, **arrow_config)
-        await self.send_and_rm(source_path, dest_path, ctx)
+        await self.send_and_rm(source_path, dest_path, interaction)
 
-    @thumbnail.command(aliases=["shorts"])
-    async def short(self, ctx, *, args):
-        args_separated = args.split(" ")
-        text = " ".join(args_separated[:-1])
-        possible_reactions = ["joy", "angry", "shrug", "smug", "think"]
-        reaction = "joy"
-        if args_separated[-1] in possible_reactions:
-            reaction = args_separated[-1]
-        else:
-            text += f" {args_separated[-1]}"
-
+    @thumbnail.command(name="short", description="A thumbnail for a short")
+    @discord.app_commands.describe(text="The text for the thumbnail.",
+                                   reaction="Doremy's reaction.")
+    async def short(self,
+                    interaction: discord.Interaction,
+                    text: str,
+                    reaction: Optional[Literal["joy", "angry", "shrug", "smug", "think"]] = "joy"):
+        await interaction.response.send_message(
+            content="Working on it...",
+            ephemeral=True
+        )
         tmp_path = os.path.abspath(os.path.dirname(__file__)) + "/../../tmp"
         if not os.path.exists(tmp_path):
             os.mkdir(tmp_path)
@@ -202,12 +152,14 @@ class ThumbnailCog(commands.Cog):
         rand_id = randint(0, 1000000)
         dest_path = f"{tmp_path}/thumbnail-{rand_id}.png"
         util.image.make_shorts_thumbnail(text, reaction, dest_path)
-        await self.send_and_rm("", dest_path, ctx)
+        await self.send_and_rm("", dest_path, interaction)
 
     @staticmethod
-    async def send_and_rm(source_path, dest_path, ctx):
+    async def send_and_rm(source_path: str, dest_path: str, interaction: discord.Interaction):
         fp = open(dest_path, "rb")
-        await ctx.send(file=discord.File(fp, filename="thumbnail.png"))
+        await interaction.channel.send(
+            file=discord.File(fp, filename="thumbnail.png")
+        )
         fp.close()
         if os.path.exists(source_path):
             os.remove(source_path)
