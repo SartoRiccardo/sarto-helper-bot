4a06838624731558452453c0043b061cc4c65d51
SartoRiccardo <rikki.sarto@gmail.com>
2023-08-14 17:22:19 +0200

    Better information

--- modules/cogs/reditor_bg.py
+++ modules/cogs/reditor_bg.py
@@ -304,23 +304,23 @@ class REditorTasksCog(commands.Cog):
         if category is None:
             return
 
-        total_ready = 0
+        total_uploaded = 0
         now = datetime.now()
         last_upload_t = await pgsql.owner.get_config("rdt_last-upload")
         if last_upload_t is not None:
             last_upload = datetime.fromtimestamp(int(last_upload_t))
-            total_ready = max(math.ceil((last_upload-now).seconds/86400), 0)
+            total_uploaded = max(math.ceil((last_upload-now).seconds/86400), 0)
 
         uploadable = await pgsql.reditor.get_uploadable()
         exportable = await pgsql.reditor.get_exportable()
-        total_ready += len(uploadable) + len(exportable)
+        total_ready = len(uploadable) + len(exportable)
         emoji = "🟢"
-        if total_ready == 0:
+        if total_uploaded == 0:
             emoji = "🔴"
-        elif total_ready == 1:
+        elif total_uploaded == 1:
             emoji = "🟡"
 
-        await category.edit(name=f"{emoji}・REditor ({total_ready})")
+        await category.edit(name=f"{emoji}・REditor ({total_uploaded}/{total_ready})")
 
 
 async def setup(bot):
